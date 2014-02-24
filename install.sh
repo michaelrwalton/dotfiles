7d38ce1da6241a1b676d935c4dd632d9e8abcc28
Michael Walton <me@michaelrwalton.com>
2014-02-24 17:42:38 -0500

    Added install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#/bin/sh bash
+
+files="vimrc gitconfig gitignore_global"
+locals="gitconfig"
+
+echo "Files to be linked: $files"
+
+for file in $files
+do
+    echo "Linking $file"
+    ln -s ~/dotfiles/$file ~/.$file
+done
+
+echo "Creating local files"
+for local in $locals 
+do
+    echo "Creating $local.local"
+    touch ~/.$local.local
+done
